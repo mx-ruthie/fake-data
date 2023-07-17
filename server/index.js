de6390c677e7d5f8fae4b17be569eef52a8dc4ad
Ruthie Irvin <ruth.diane.irvin@gmail.com>
2023-07-17 15:57:55 -0500

    migration success

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 import express from "express";
 import cors from "cors";
 import { PrismaClient } from "@prisma/client";
+import dotenv from 'dotenv'
 
 const prisma = new PrismaClient()
 
+dotenv.config()
 const app = express();
 const PORT = 8080;
 
 app.use(cors());
+
+console.log("checking for env", process.env.DATABASE_URL)
 
 app.listen(PORT, () => console.log(`Hola! Server is running on port ${PORT}`));
