044b989cff7c4b510edf5b188d07ac9eb213f3fb
Ruthie Irvin <ruth.diane.irvin@gmail.com>
2023-07-20 16:09:55 -0500

    http working tracing not so much

--- server/tracing.cjs
+++ server/tracing.cjs
@@ -1,42 +1,25 @@
 // tracing.js
 'use strict';
-
-const {dotenv} = require('dotenv');
-const { HoneycombSDK } = require('@honeycombio/opentelemetry-node');
-const {
-  getNodeAutoInstrumentations,
-} = require('@opentelemetry/auto-instrumentations-node');
-const { NodeTracerProvider } = require('@opentelemetry/sdk-trace-node');
-const { registerInstrumentations } = require('@opentelemetry/instrumentation');
-const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
-const { ExpressInstrumentation } = require('@opentelemetry/instrumentation-express');
-
-const provider = new NodeTracerProvider();
-provider.register();
 require('dotenv').config();
 
-// import {fetch} from "node-fetch";
+const { HoneycombSDK } = require('@honeycombio/opentelemetry-node');
+const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
+const { PrismaInstrumentation } = require('@prisma/instrumentation');
 
 // uses the HONEYCOMB_API_KEY and OTEL_SERVICE_NAME environment variables
 const sdk = new HoneycombSDK({
-    instrumentations: [
-        // Disable fs auto-instrumentation to avoid noise and overhead during startup
-        getNodeAutoInstrumentations({
+    instrumentations: [    
+        //new HttpInstrumentation(),
+        //new ExpressInstrumentation(),
+        new PrismaInstrumentation(),
+            getNodeAutoInstrumentations({
+            // we recommend disabling fs autoinstrumentation since it can be noisy
+            // and expensive during startup
             '@opentelemetry/instrumentation-fs': {
                 enabled: false,
             },
         }),
-
-        // Register additional instrumentations
-        registerInstrumentations({
-            instrumentations: [
-                // Express instrumentation expects HTTP layer to be instrumented
-                new HttpInstrumentation(),
-                new ExpressInstrumentation(),
-            ],
-        }),
     ],
 });
-
 
 sdk.start();
