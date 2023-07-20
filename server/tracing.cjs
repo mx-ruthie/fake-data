7e0dcddc7b7842a77b8a2fb786b4ffbbdc12c37c
Ruthie Irvin <ruth.diane.irvin@gmail.com>
2023-07-20 10:50:45 -0500

    adds express otel int

--- server/tracing.cjs
+++ server/tracing.cjs
@@ -6,22 +6,37 @@ const { HoneycombSDK } = require('@honeycombio/opentelemetry-node');
 const {
   getNodeAutoInstrumentations,
 } = require('@opentelemetry/auto-instrumentations-node');
+const { NodeTracerProvider } = require('@opentelemetry/sdk-trace-node');
+const { registerInstrumentations } = require('@opentelemetry/instrumentation');
+const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
+const { ExpressInstrumentation } = require('@opentelemetry/instrumentation-express');
 
+const provider = new NodeTracerProvider();
+provider.register();
 require('dotenv').config();
 
 // import {fetch} from "node-fetch";
 
 // uses the HONEYCOMB_API_KEY and OTEL_SERVICE_NAME environment variables
 const sdk = new HoneycombSDK({
-    instrumentations: [    
+    instrumentations: [
+        // Disable fs auto-instrumentation to avoid noise and overhead during startup
         getNodeAutoInstrumentations({
-            // we recommend disabling fs autoinstrumentation since it can be noisy
-            // and expensive during startup
             '@opentelemetry/instrumentation-fs': {
                 enabled: false,
             },
         }),
+
+        // Register additional instrumentations
+        registerInstrumentations({
+            instrumentations: [
+                // Express instrumentation expects HTTP layer to be instrumented
+                new HttpInstrumentation(),
+                new ExpressInstrumentation(),
+            ],
+        }),
     ],
 });
+
 
 sdk.start();
